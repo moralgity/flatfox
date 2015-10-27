54ea500c424cba903fc3b46217467e1479545041
moralgity <comfy@cock.li>
2015-10-27 07:12:13 +0000

    commiting files

---
+++ link.sh
@@ -0,0 +1,18 @@
+# This script replaces all your .files to fit the flatfox theme
+#!/bin/sh
+
+# Remove the files that will be replaced
+
+rm -rf ~/.Xresources ~/.config/gtk-2.0/ ~/.config/gtk-3.0/ ~/.config/openbox/ ~/.config/sxhkd/ ~/.themes/flatfox ~/.vimrc ~/.xinirtc ~/images/wallpapers/moshi-moshi-fox-desu.jpg
+
+#link the files
+
+cp Xresources ~/.Xresources 
+cp -ar config/gtk-2.0/ ~/.config/gtk-2.0/ 
+cp -ar config/gtk-3.0/ ~/.config/gtk-3.0/ 
+cp -ar config/openbox/ ~/.config/openbox/ 
+cp -ar config/sxhkd/ ~/.config/sxhkd/
+cp -ar themes/flatfox ~/.themes/flatfox
+cp vimrc ~/.vimrc
+cp xinitrc ~/.xinirc
+cp moshi-moshi-fox-desu.jpg ~/images/wallpapers/moshi-moshi-fox-desu.jpg
---
+++ vimrc
@@ -0,0 +1,31 @@
+"" MAKE USABLE
+
+" Enable syntax
+syntax on
+
+" fish fix
+set shell=bash
+
+" Hide statusbar
+" set noshowmode
+
+" Lines above/below cursor
+set scrolloff=2
+
+" Indentation
+set tabstop=2
+set softtabstop=2
+
+"" RICE
+
+" Enable line numbers
+set number
+
+"" MISC
+
+" Disable backups
+set nobackup
+set nowritebackup
+set noswapfile
+set noundofile
+
